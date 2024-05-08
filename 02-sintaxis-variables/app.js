5a5b6d63f0c2972602f90d5fcb4696f4ef76b9d3
afsanchez04 <afsanchez04@gmail.com>
2024-05-08 11:33:21 -0500

    feat: sintaxis-variables

---
+++ 02-sintaxis-variables/app.js
@@ -0,0 +1,42 @@
+
+/* Repaso clase 1 */
+
+/* Salidas */
+// alert('hola')
+// console.log('se imprime en consola')
+// document.write('se imprime en el viewtport del navegador')
+
+
+/* Entradas */
+//prompt('dime tu edad')
+
+/* Sintaxis */
+
+/* Formas correctas de declarar */
+
+// var nombre = 'pepe';
+// let nombreCiudad = 'medellin';
+// const color_camiseta = 'rojo';
+// const $vidas = 5;
+
+/* Formas incorrectas de nombrar variables */
+
+// var 2vidas = 'vidas';
+// var nuevo color = 'verde';
+// var nuevo-color = 'azul';
+
+/* String o texto */
+// 'string'
+// "string"
+
+/* numerico */
+// 3
+// 123
+// 12.4
+
+/* El + tiene dos funciones en javascript: sumar o concatenar (unir) */
+var resultado = 8+"2"+3+"4";
+
+console.log(resultado) // 82
+
+
