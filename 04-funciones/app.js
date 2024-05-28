f28122d6dad9772fbd09d6c4373a426bec1a6b8b
afsanchez04 <afsanchez04@gmail.com>
2024-05-28 11:19:54 -0500

    feat: DOM

---
+++ 04-funciones/app.js
@@ -0,0 +1,38 @@
+
+
+/* function mezclar (color1, color2){
+    return 'la suma de los colores da: ' + (color1 + color2)
+}   
+
+mezclar(); */
+
+//Primero crear la función
+function saludar (){
+    return 'hola mundo';
+}
+
+//Segundo se llama la función
+console.log( saludar() ) 
+
+let mensaje = `Saludo: ${saludar()} javascript`; 
+console.log( mensaje ); //
+
+
+function bienvenida (nombre, pais){
+    return 'Bienvenido ' + nombre + ' a tu país ' + pais;
+}
+
+let result = bienvenida ('Carlos', 'Colombia');
+
+console.log(result)
+
+
+//Función que me dice la fecha:
+
+function mostrarFecha(fecha){
+    alert( `El día de la semana es: ${fecha}` );
+    return `El día de la semana es: ${fecha}`; 
+}
+
+/* let fecha = mostrarFecha('martes');
+console.log(fecha); */
